ed581db201ea5d66e29ff6c07c34905283d4b644
Ben <bencrocker22@outlook.com>
2024-10-14 23:33:26 +0100

    Initial commit

---
+++ NumberPlateGenerator/NumberPlateGenerator.cs
@@ -0,0 +1,139 @@
+﻿using System.Data;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("NumberPlateGeneratorTests")]
+namespace NumberPlateGeneration
+{
+    public class NumberPlateGenerator
+    {
+        const int CUT_OFF_MONTH = 3; // Month that marks the start of a new year
+        const int HALFWAY_CUT_OFF_MONTH = 9; // Month that marks halfway through the year
+        const int LATE_YEAR_IDENTIFIER = 50; // Value to add to age identifier if numberplate is halfway through the year or further
+        const int NUMBER_OF_RANDOM_LETTERS = 3; 
+        const int MEMORY_TAG_LENGTH = 2;
+
+        int NumberOfValidLetters { get { return _validLetters.Count; } }
+        internal int MaximumCombinationOfRandomCharacters { get { return (int)Math.Pow((double) NumberOfValidLetters, (double) NUMBER_OF_RANDOM_LETTERS); } }
+
+        readonly IReadOnlyList<char> _validLetters = new List<char> { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y' };
+        readonly IReadOnlyList<char> _validDigits = new List<char> { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        Dictionary<string, int> _usedNumberplates = new Dictionary<string, int>(); 
+
+        public string GenerateNumberPlate(string memoryTag, DateTime date)
+        {
+            if (!IsValidMemoryTag(memoryTag))
+                throw new ArgumentException("Memory Tag is not valid for this format", nameof(memoryTag));
+
+            string numberPlateStart = memoryTag + GetAgeIdentifier(date);
+
+            return GenerateNumberPlateForIdentifier(numberPlateStart);
+        }
+
+        public bool IsNumberPlateValid(string numberPlate)
+        {
+            if (numberPlate.Length != 8)
+                return false;
+
+            return 
+                IsValidLetter(numberPlate[0]) &&
+                IsValidLetter(numberPlate[1]) &&
+                IsValidDigit(numberPlate[2]) &&
+                IsValidDigit(numberPlate[3]) &&
+                numberPlate[4] == ' ' &&
+                IsValidLetter(numberPlate[5]) &&
+                IsValidLetter(numberPlate[6]) &&
+                IsValidLetter(numberPlate[7]);
+        }
+
+        string GenerateNumberPlateForIdentifier(string identifier)
+        {
+            int numberPlateIndex = _usedNumberplates.GetValueOrDefault(identifier, 0);
+
+            if (numberPlateIndex >= MaximumCombinationOfRandomCharacters)
+                throw new ConstraintException($"Exceeded number of numberplates for: {identifier}");
+
+            string numberPlateEnd = GetNextNumberplateEnding(NUMBER_OF_RANDOM_LETTERS, numberPlateIndex);
+
+            _usedNumberplates[identifier] = numberPlateIndex + 1;
+
+            return identifier + " " + numberPlateEnd;
+        }
+
+        string GetAgeIdentifier(DateTime date)
+        {
+            if (!IsValidDate(date))
+                throw new ArgumentException("Date is not valid for this format", nameof(date));
+
+            int year = int.Parse(date.ToString("yy")); // Take only the last 2 digits from the year
+
+            if (ShouldUseEarlyYearIdentifier(date))
+                return year.ToString("D2");
+            else
+            {
+                if (date.Month < CUT_OFF_MONTH)
+                    return (year + LATE_YEAR_IDENTIFIER - 1).ToString();
+                else
+                    return (year + LATE_YEAR_IDENTIFIER).ToString();
+            } 
+        }
+
+        string GetNextNumberplateEnding(int numberOfLetters, int numberPlateIndex)
+        {
+            string letterSequence = "";
+            int remainingUnitsToMap = numberPlateIndex;
+
+            for (int i = 0; i < numberOfLetters; i++)
+            {
+                letterSequence = _validLetters[remainingUnitsToMap % NumberOfValidLetters] + letterSequence;
+                remainingUnitsToMap /= NumberOfValidLetters;
+
+                if (remainingUnitsToMap == 0)
+                {
+                    letterSequence = letterSequence.PadLeft(numberOfLetters, _validLetters[0]);
+
+                    return letterSequence;
+                }
+            }
+
+            return letterSequence;
+        }
+
+        bool IsValidLetter(char c)
+        {
+            return _validLetters.Contains(c);
+        }        
+        
+        bool IsValidDigit(char c)
+        {
+            return _validDigits.Contains(c);
+        }
+
+        bool IsValidMemoryTag(string memoryTag)
+        {
+            if (memoryTag.Length != MEMORY_TAG_LENGTH)
+                return false;
+
+            foreach(char c in memoryTag)
+            {
+                if (!_validLetters.Contains(c))
+                    return false;
+            }
+
+            return true;
+        }
+
+        bool IsValidDate(DateTime date)
+        {
+            DateTime earliestDate = new DateTime(2001, HALFWAY_CUT_OFF_MONTH, 01);
+            DateTime latestDate = new DateTime(2051, CUT_OFF_MONTH, 01);
+
+            return date >= earliestDate && date < latestDate;
+        }
+
+        bool ShouldUseEarlyYearIdentifier(DateTime date)
+        {
+            return date.Month is >= CUT_OFF_MONTH and < HALFWAY_CUT_OFF_MONTH;
+        }
+    }
+}
---
+++ NumberPlateGeneratorTests/NumberPlateGeneratorTests.cs
@@ -0,0 +1,186 @@
+﻿using NumberPlateGeneration;
+using System.Data;
+
+namespace NumberPlateGeneratorTests
+{
+    public class NumberPlateGeneratorTests
+    {
+        [InlineData("AB10 DKM")]
+        [InlineData("MV10 FRH")]
+        [InlineData("YA51 YHL")]
+        [Theory]
+        public void IsNumberPlateValid_ValidNumberPlates_ReturnsTrue(string numberPlate)
+        {
+            NumberPlateGenerator numberPlateGenerator = new NumberPlateGenerator();
+
+            bool isValid = numberPlateGenerator.IsNumberPlateValid(numberPlate);
+
+            Assert.True(isValid);
+        }
+
+        [InlineData("AA10 AAA ")]
+        [InlineData("AA10AAA ")]
+        [InlineData("ABC0 ABC")]
+        [InlineData("A012 ABC")]
+        [InlineData("0A12 ABC")]
+        [InlineData("AB12 0BC")]
+        [InlineData("AB12 A0C")]
+        [InlineData("AB12 AB0")]
+        [InlineData("AB12 ABI")]
+        [InlineData("AQ12 ABC")]
+        [InlineData("ZB12 ABC")]
+        [Theory]
+        public void IsNumberPlateValid_InvalidNumberPlates_ReturnsFalse(string numberPlate)
+        {
+            NumberPlateGenerator numberPlateGenerator = new NumberPlateGenerator();
+
+            bool isValid = numberPlateGenerator.IsNumberPlateValid(numberPlate);
+
+            Assert.False(isValid);
+        }
+
+        [InlineData("AA", 2010, 1, 1)]
+        [InlineData("RL", 2003, 8, 12)]
+        [InlineData("JJ", 2019, 3, 24)]
+        [InlineData("SA", 2017, 4, 30)]
+        [InlineData("BP", 2011, 6, 28)]
+        [Theory]
+        public void GenerateNumberPlate_NumberPlateValidity_StartsWithMemoryTag(string memoryTag, int year, int month, int day)
+        {
+            NumberPlateGenerator numberPlateGenerator = new NumberPlateGenerator();
+
+            string numberPlate = numberPlateGenerator.GenerateNumberPlate(memoryTag, new DateTime(year, month, day));
+
+            bool startsWithMemoryTagAndYear = numberPlate.StartsWith(memoryTag);
+
+            Assert.True(startsWithMemoryTagAndYear);
+        }
+
+        [InlineData("AA", 2010, 1, 1)]
+        [InlineData("RL", 2003, 8, 12)]
+        [InlineData("JJ", 2019, 3, 24)]
+        [InlineData("SA", 2017, 4, 30)]
+        [InlineData("BP", 2011, 6, 28)]
+        [Theory]
+        public void GenerateNumberPlate_NumberPlateValidity_IsValid(string memoryTag, int year, int month, int day)
+        {
+            NumberPlateGenerator numberPlateGenerator = new NumberPlateGenerator();
+
+            string numberPlate = numberPlateGenerator.GenerateNumberPlate(memoryTag, new DateTime(year, month, day));
+
+            bool isValid = numberPlateGenerator.IsNumberPlateValid(numberPlate);
+
+            Assert.True(isValid);
+        }
+
+        [InlineData(2003, 3, 1, "03")]
+        [InlineData(2003, 4, 1, "03")]
+        [InlineData(2003, 5, 1, "03")]
+        [InlineData(2003, 6, 1, "03")]
+        [InlineData(2003, 7, 1, "03")]
+        [InlineData(2003, 8, 1, "03")]
+        [InlineData(2011, 5, 21, "11")]
+        [InlineData(2039, 7, 6, "39")]
+        [InlineData(2007, 8, 29, "07")]
+        [Theory]
+        public void GenerateNumberPlate_EarlyYear_EarlyYearCode (int year, int month, int day, string expected)
+        {
+            NumberPlateGenerator numberPlateGenerator = new NumberPlateGenerator();
+            DateTime date = new DateTime(year, month, day);
+
+            string numberPlate = numberPlateGenerator.GenerateNumberPlate("AA", date);
+
+            string ageIdentifier = numberPlate.Substring(2, 2);
+
+            Assert.Equal(expected, ageIdentifier);
+        }
+
+        [InlineData(2003, 9, 1, "53")]
+        [InlineData(2003, 10, 1, "53")]
+        [InlineData(2003, 11, 1, "53")]
+        [InlineData(2003, 12, 1, "53")]
+        [InlineData(2004, 1, 1, "53")]
+        [InlineData(2004, 2, 1, "53")]
+        [InlineData(2025, 11, 28, "75")]
+        [InlineData(2014, 9, 11, "64")]
+        [InlineData(2021, 2, 9, "70")]
+        [Theory]
+        public void GenerateNumberPlate_LateYear_EarlyYearCode(int year, int month, int day, string expected)
+        {
+            NumberPlateGenerator numberPlateGenerator = new NumberPlateGenerator();
+            DateTime date = new DateTime(year, month, day);
+
+            string numberPlate = numberPlateGenerator.GenerateNumberPlate("AA", date);
+
+            string ageIdentifier = numberPlate.Substring(2, 2);
+
+            Assert.Equal(expected, ageIdentifier);
+        }
+
+        [InlineData("aA")]
+        [InlineData("11")]
+        [InlineData("BBB")]
+        [InlineData("AI")]
+        [InlineData("AQ")]
+        [InlineData("AZ")]
+        [Theory]
+        public void GenerateNumberPlate_InvalidMemoryTags_ThrowsArgumentException(string memoryTag)
+        {
+            NumberPlateGenerator numberPlateGenerator = new NumberPlateGenerator();
+
+            DateTime date = new DateTime(2010, 3, 5);
+
+            Assert.Throws<ArgumentException>(() => numberPlateGenerator.GenerateNumberPlate(memoryTag, date));
+        }
+
+        [InlineData(2001, 8, 1)]
+        [InlineData(2001, 8, 31)]
+        [InlineData(2051, 3, 1)]
+        [Theory]
+        public void GenerateNumberPlate_InvalidDates_ThrowsArgumentException(int year, int month, int day)
+        {
+            NumberPlateGenerator numberPlateGenerator = new NumberPlateGenerator();
+
+            DateTime date = new DateTime(year, month, day);
+
+            Assert.Throws<ArgumentException>(() => numberPlateGenerator.GenerateNumberPlate("AA", date));
+        }
+
+        [Fact]
+        public void GenerateNumberPlate_ExceedingPossibleCombinations_ThrowsConstraintException()
+        {
+            NumberPlateGenerator numberPlateGenerator = new NumberPlateGenerator();
+
+            for (int i = 0; i < numberPlateGenerator.MaximumCombinationOfRandomCharacters; i++)
+            {
+                numberPlateGenerator.GenerateNumberPlate("AA", new DateTime(2002, 3, 1));
+            }
+
+            Assert.Throws<ConstraintException>(() => numberPlateGenerator.GenerateNumberPlate("AA", new DateTime(2002, 3, 1)));
+
+            for (int i = 0; i < numberPlateGenerator.MaximumCombinationOfRandomCharacters; i++)
+            {
+                numberPlateGenerator.GenerateNumberPlate("BB", new DateTime(2002, 3, 1));
+            }
+
+            Assert.Throws<ConstraintException>(() => numberPlateGenerator.GenerateNumberPlate("BB", new DateTime(2002, 3, 1)));
+        }
+
+        [Fact]
+        public void GenerateNumerPlate_GeneratingAllCombinations_AllAreUnique()
+        {
+            NumberPlateGenerator numberPlateGenerator = new NumberPlateGenerator();
+
+            HashSet<string> usedNumberplates = new HashSet<string>();
+
+            for (int i = 0; i < numberPlateGenerator.MaximumCombinationOfRandomCharacters; i++)
+            {
+                string numberPlate = numberPlateGenerator.GenerateNumberPlate("DE", new DateTime(2012, 10, 1));
+
+                bool isUnique = usedNumberplates.Add(numberPlate);
+
+                Assert.True(isUnique);
+            }
+        }
+    }
+}
